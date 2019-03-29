6c136ecc636aa86e548fe9258ee82912abce5cf0
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-03-29 17:18:17 +0800

    Add styles for the recording button

--- src/dictaphone.js
+++ src/dictaphone.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import SpeechRecognition from 'react-speech-recognition'
+import styles from './dictaphone.module.css';
 
 const Dictaphone = ({transcript, resetTranscript, recognition, startListening, stopListening}) => {
     recognition.lang = 'id-ID'
 
     const toggleDictaphone = (event) => {
-        event.target.checked? startListening() : stopListening()
+        if(event.target.checked){
+            resetTranscript()
+            startListening()
+        }
+        else{
+            stopListening()
+        }
     }
 
     return (
         <div>
-            <input type="checkbox" onChange={toggleDictaphone}></input>
-            <button onClick={resetTranscript}>Reset</button>
+            <input className={`${styles.tgl} ${styles.tgl_flip}`} id="cb5" type="checkbox" onChange={toggleDictaphone}/>
+            <label className={styles.tgl_btn} data-tg-off="Off" data-tg-on="On" htmlFor="cb5"></label>
             <br></br>
             <span>{transcript}</span>
         </div>)
