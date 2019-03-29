c8abdf3ca4ee10b37e8d5dfb382f70789b38cbf0
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-03-29 16:29:22 +0800

    Adding basic implementation of speech to text

--- src/test.js
+++ src/test.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import SpeechRecognition from 'react-speech-recognition'
 
-const Test = () => (
-    <div>Hello there</div>
-)
+const Dictaphone = ({transcript, resetTranscript, recognition, startListening, stopListening}) => {
+    recognition.lang = 'id-ID'
 
-export default Test
+    const toggleDictaphone = (event) => {
+        event.target.checked? startListening() : stopListening()
+    }
+
+    return (
+        <div>
+            <input type="checkbox" onChange={toggleDictaphone}></input>
+            <button onClick={resetTranscript}>Reset</button>
+            <br></br>
+            <span>{transcript}</span>
+        </div>)
+}
+
+export default SpeechRecognition({autoStart: false})(Dictaphone)
