3db848534035230cd3093dd620a2a99c1847fe64
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-04-24 16:28:04 +0800

    Add stateful higher order component for chatbox

--- src/chat.js
+++ src/chat.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react'
 
-const ChatBox = ({userValue="", chatHistory="", userValueChangeHandler=() => {}, submitHandler=() => {}}) => {
+
+class ChatBoxComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            chatHistory: ""
+        }
+        this.handleSubmission = this.handleSubmission.bind(this)
+    }
+
+    handleSubmission(chatMessage){
+        this.setState({
+            chatHistory: chatMessage + "\n" + this.state.chatHistory
+        })
+    }
+
+    render() {
+        return (
+            <ChatBox chatHistory={this.state.chatHistory} submitHandler={this.handleSubmission}></ChatBox>
+        )
+    }
+}
+
+const ChatBox = ({chatHistory="", submitHandler=() => {}}) => {
+    let _chatMessage
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        submitHandler(_chatMessage.value)
+        _chatMessage.value = ""
+    }
+
     return(
         <div>
-            <form onSubmit={submitHandler}>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group row">
                     <input type="text" className="form-control input-sm col-sm-8"
-                        value={userValue} onVolumeChange={userValueChangeHandler}/>
+                        ref={input => _chatMessage = input}/>
                     <input type="submit" className="col-sm-3 btn btn-primary form-default-button" value="Send"/>
                 </div>
-                <textarea className="form-control input-sm col-sm-11" value={chatHistory}></textarea>
+                <textarea className="form-control input-sm col-sm-11" readOnly value={chatHistory}></textarea>
             </form>
         </div>
     )
 }
 
-export default ChatBox
+export default ChatBoxComponent
